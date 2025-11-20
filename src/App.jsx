d370e7823c6cf42e1b83bb837a3a3cba76fa5127
Tamara-nbiyeva <tamaranbiyeva26@gmail.com>
2025-11-20 11:23:20 +0400

    Şekiller elave edildi + router düzeldildi

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import { Routes, Route } from "react-router-dom"
 import Home from "./pages/Home"
+import SignIn from "./pages/SignIn"
 import SignUp from "./pages/SignUp"
 import './App.css'
 
@@ -10,8 +11,8 @@ function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      
-      <Route path="/signup" element={<SignUp />} />
+      <Route path="/login" element={<SignIn />} />
+      <Route path="/register" element={<SignUp />} />
     </Routes>
   )
 }
