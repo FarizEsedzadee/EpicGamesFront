2c715c103455c43b72d6e7e69d6d132d4f719ff4
FarizEsedzadee <fariz.esedov.99@gmail.com>
2025-11-20 12:20:37 +0400

    -- fix back button

--- src/components/ui/BackButton.jsx
+++ src/components/ui/BackButton.jsx
@@ -3,6 +3,6 @@ import { RiArrowLeftSLine } from "react-icons/ri";
 
 export default function BackButton() {
     return (
-        <div className='flex items-center gap-1 my-[12px_24px] cursor-pointer group'> <RiArrowLeftSLine className='text-[20px]' /> <span className='text-[14px] font-medium group-hover:translate-x-[5px] transition-all duration-300'>Geri</span> </div>
+        <div className='flex text-white items-center gap-1 my-[12px_24px] cursor-pointer group'> <RiArrowLeftSLine className='text-[20px]' /> <span className='text-[14px] font-medium group-hover:translate-x-[5px] transition-all duration-300'>Geri</span> </div>
     )
 }
