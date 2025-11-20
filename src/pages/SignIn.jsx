28278747efba096ea671ebf138a541092e3ce9ae
Tamara-nbiyeva <tamaranbiyeva26@gmail.com>
2025-11-20 12:45:44 +0400

    logini duzeltmeye calisiram

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -4,31 +4,36 @@ import xbox from "../../public/x-box.png";
 import nintendo from "../../public/nintendo.png";
 
 export default function SignIn() {
-    return (
-        <div>
-            <div>
-                <h1>Epic games'e giriş yap</h1>
-            </div>
-            <div>
-                <div>
-                    <p>Sadece konsoldamı oynadın? İlerleme durumlarına ve satın alımlar için giriş yap.</p>
-                    <a href="" class="w-[430px]">
-                        <img src={playstationImg} alt="Playstation" class="w-[40px] h-[40px]" />
-                    </a>
-                    <a href="" class="w-[430px]">
-                        <img src={xbox} alt="xbox-logo" class="w-[40px] h-[40px]" />
-                    </a>
-                    <a href="" class="">
-                        <img src={nintendo} alt="nintendo" class="w-[40px] h-[40px]" />
-                    </a>
-                </div>
-                <div></div>
-                <div></div>
-                <div></div>
-            </div>
-            <div></div>
+  return (
+    <div className='mx-auto flex flex-col gap-[16px] items-center justify-center w-[561px] h-[1395px] bg-[#18181c] rounded-[25px]'>
+      <div >
+        <h1 className="text-white text-[24px] font-bold">Epic Games'e giriş yap</h1>
+      </div>
+      <div className='flex flex-col gap-[16px] items-center justify-center w-[465px] h-[285px] border border-gray-700 rounded-[15px] p-[16px]'>
+        <p className=' text-center text-[#7b7b7d] font-semibold'>Sadece konsoldamı oynadın? İlerleme durumlarına ve satın alımlarına erişmek için giriş yap.</p>
+        <a href="" className='flex items-center w-full bg-[#202024] p-[5px] rounded-[40px]'>
+          <img src={playstationImg} alt="" className='w-[40px] h-[40px] rounded-full' />
+          <p className='text-white w-full text-center'>PlayStation™Network</p>
+        </a>
+        <a href="" className='flex items-center w-full bg-[#202024] p-[5px] rounded-[40px]'>
+          <img src={xbox} alt="" className='w-[40px] h-[40px] rounded-full'/>
+          <p className='text-white w-full text-center'>Xbox ağı</p>
+        </a>
+        <a href="" className='flex items-center w-full bg-[#202024] p-[5px] rounded-[40px] '>
+          <img src={nintendo} alt="" className='w-[40px] h-[40px] rounded-full'/>
+          <p className='text-white w-full text-center'>Nintendo Hesabı</p>
+        </a>
+      </div>
+      <div className='w-[464px] h-[220px] border border-gray-700 rounded-[15px]'>
+        
 
-
-        </div>
-    )
+      </div>
+      <div className='w-[464px] h-[57px] border border-gray-700 rounded-[15px]'></div>
+      <div className='w-[464px] h-[523px] border border-gray-700 rounded-[15px]'></div>
+      <div className='flex flex-col pt-[30px] gap-[16px] w-[464px] justify-center items-center'>
+        <a href="" className='text-[#26bbff]'>Giriş yapmakta sorunmu yaşıyorsun?</a>
+        <a href="" className='text-[#26bbff]'>Gizlilik politikası</a>
+      </div>
+    </div>
+  )
 }
