6c267796b30fc451d44cde1415b54b1e5f78b6b0
Tamara-nbiyeva <tamaranbiyeva26@gmail.com>
2025-12-02 11:58:38 +0400

    Local changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,16 @@
 import Header from '@/components/Header'
 import React from 'react'
+import { Swiper, SwiperSlide } from 'swiper/react';
+import Card from '@/components/ui/Card';
+
+// Import Swiper styles
+import 'swiper/css';
 
 export default function Home() {
     return (
         <div className="min-h-screen text-white">
             <Header />
+
         </div>
     )
 }
