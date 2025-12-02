5bb73312193ee318e344da16a5728fe27dd1640d
FarizEsedzadee <fariz.esedov.99@gmail.com>
2025-12-02 12:01:53 +0400

    -home düzəldi

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -13,19 +13,9 @@ export default function Home() {
     return (
         <div className="w-full min-h-screen text-white">
             <Header />
-<<<<<<< HEAD
             <main className='sm:max-w-[770px] lg:max-w-[1050px] xl:max-w-[1185px] 2xl:max-w-[1240px] mx-auto py-3'>
-=======
-<<<<<<< HEAD
-
-        </div>
-=======
-            <main>
->>>>>>> aa022353496d499bbfbe02d75944741815464e41
                 <SecondHeader />
-
             </main>
         </div >
->>>>>>> f134ec75d31a62a7b4b8f186f18a7784e7219e4f
     )
 }
